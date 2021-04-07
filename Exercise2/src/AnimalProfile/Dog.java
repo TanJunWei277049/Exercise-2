e9499f11a92d8beb0580a4a0af00356c485776d0
Dell <Dell@DESKTOP-80ULB4K>
2021-04-07 14:19:04 +0800

    First Commit

--- Exercise2/src/AnimalProfile/Dog.java
+++ Exercise2/src/AnimalProfile/Dog.java
@@ -13,22 +13,47 @@ public class Dog {
 		
 	}
 	
+	void instruction(String i) {
+		
+		switch(i) {
+		case "Sit" : System.out.println("Sitting on the floor.");
+						break;
+		case "Bark" : System.out.println("Woof woof.");
+						break;
+		case "Lay" : System.out.println("Laying down.");
+						break;
+		case "Jump" : System.out.println("Jumping...");
+						break;
+		}
+	}
+	
 	void food(String food, double price) {
 		total = price*3*31; //price of 1 meal*3times*31 days
+		
 		System.out.println("It likes to eat " + food);
 		System.out.printf("Cost of food for a month is RM%.2f%n",total);
 	}
 	
 	void foodAmount(double f) {
 		foodWeight = f*3*31;//weight of food for 1 meal *3 times*31 days
+		
 		System.out.println("It eats " + f +"kg a meal");
 		System.out.printf("Therefore it eats %.2fkg a month",foodWeight);
 	}
 	
 	void age(int y) {
 		age = 2021-y;
+		
 		System.out.println("It was born on year " + y);
 		System.out.println("It is " + age + " years old");
+	}
+	
+	void water(int a) {
+		
+		if(a>=5)
+			System.out.println("It should drink at least 5 litre of water.");
+		else 
+			System.out.println("It should drink at least 2 litre of water.");
 	}
 	
 	
--- Exercise2/src/AnimalProfile/Main.java
+++ Exercise2/src/AnimalProfile/Main.java
@@ -10,30 +10,38 @@ public class Main {
 		
 		System.out.println("Information for Bulldog");
 		bulldog.printInfo("John", "Yellow", 'M', 30.2, 23.3);
+		bulldog.instruction("Jump");
 		bulldog.food("Blueberries", 2.1);
 		bulldog.foodAmount(1.2);
 		bulldog.age(2018);
+		bulldog.water(3);
 		
 		System.out.println("\n--------------------------------------------------");
 		System.out.println("Information for Poodel");
 		poodle.printInfo("Mary", "White", 'F', 41.1, 32.3);
+		poodle.instruction("Lay");
 		poodle.food("Fish Meat", 3.5);
 		poodle.foodAmount(1.5);
 		poodle.age(2017);
+		poodle.water(4);
 		
 		System.out.println("\n--------------------------------------------------");
 		System.out.println("Information for Boxer");
 		boxer.printInfo("Lucky", "Brown", 'M', 55.1, 29.5);
+		boxer.instruction("Bark");
 		boxer.food("Blackberries", 2.3);
 		boxer.foodAmount(2.3);
 		boxer.age(2009);
+		boxer.water(12);
 		
 		System.out.println("\n--------------------------------------------------");
 		System.out.println("Information for Rottweiler");
 		rottweiler.printInfo("May", "Black", 'F', 56.8, 40.7);
+		rottweiler.instruction("Sit");
 		rottweiler.food("Chicken Meat", 3.9);
 		rottweiler.foodAmount(2.9);
 		rottweiler.age(2013);
+		rottweiler.water(8);
 
 	}
 
